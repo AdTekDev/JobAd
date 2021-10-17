72dd80b2f5930480c7c553b8272f2bddd027b389
AdTekDev <nguyenngtu777@gmail.com>
2021-10-17 09:29:35 +0700

    3

--- app.py
+++ app.py
@@ -2,11 +2,11 @@ from flask import Flask , render_template
 
 app = Flask(__name__ , template_folder='views')
 
-@app.route("/")
+@app.route("/", methods=['GET', 'POST'])
 def main():
     return render_template('index.html')
 
-@app.route("/jobs")
+@app.route("/jobs" , methods=['GET', 'POST'])
 def jobs():
     jobs = [
         { "title": "IT Admin", "describe" : "Fullstack Skills !" } ,
